681422a174a32d3e5d5766623c99c25b7cd9524f
Anders Andersson <trezker@gmail.com>
2010-10-23 20:56:09 +0200

    Added functions to convert to and from strings

--- sinxml/sinxml.h
+++ sinxml/sinxml.h
@@ -3,5 +3,31 @@
 
 #include "document.h"
 #include "element.h"
+#include <sstream>
+
+namespace sinxml
+{
+
+template<typename T>
+std::string tostring(T& t)
+{
+	std::stringstream c;
+	c<<t;
+	std::string s;
+	c>>s;
+	return s;
+}
+
+template<typename T>
+T fromstring(const std::string& s)
+{
+	std::stringstream c;
+	c<<s;
+	T t;
+	c>>t;
+	return t;
+}
+
+}
 
 #endif
